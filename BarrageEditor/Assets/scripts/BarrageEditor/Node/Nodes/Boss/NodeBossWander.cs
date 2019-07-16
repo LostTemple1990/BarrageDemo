bf55ce5fa2796b1d9cc9fbb881e0190874d608af
LostTemple1990 <372137453@qq.com>
2019-07-16 19:46:26 +0800

    add test stage and spell card

---
+++ BarrageEditor/Assets/scripts/BarrageEditor/Node/Nodes/Boss/NodeBossWander.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using YKEngine;
+
+namespace BarrageEditor
+{
+    public class NodeBossWander : BaseNode
+    {
+        public override void Init(RectTransform parentTf)
+        {
+            _nodeType = NodeType.BossWander;
+            base.Init(parentTf);
+            _functionImg.sprite = ResourceManager.GetInstance().GetSprite("NodeIcon", "bosswander");
+        }
+
+        public override void CreateDefaultAttrs()
+        {
+            BaseNodeAttr nodeAttr;
+            // Boss
+            nodeAttr = NodeManager.CreateNodeAttr(NodeAttrType.Any);
+            nodeAttr.Init(this, "Unit", null);
+            attrs.Add(nodeAttr);
+            // 走动时间
+            nodeAttr = NodeManager.CreateNodeAttr(NodeAttrType.Any);
+            nodeAttr.Init(this, "Duration(in frame)", null);
+            attrs.Add(nodeAttr);
+        }
+
+        public override string GetNodeName()
+        {
+            return "boss wander";
+        }
+
+        public override string ToDesc()
+        {
+            return string.Format("{0} wander in {1} frame(s)", GetAttrByIndex(0).GetValueString(), GetAttrByIndex(1).GetValueString());
+        }
+
+        public override string ToLuaHead()
+        {
+            return string.Format("{0}:Wander({1})\n", GetAttrByIndex(0).GetValueString(), GetAttrByIndex(1).GetValueString());
+        }
+    }
+}
---
+++ BarrageEditor/Assets/scripts/BarrageEditor/Node/Nodes/Boss/NodeSetBossWanderProps.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using YKEngine;
+
+namespace BarrageEditor
+{
+    public class NodeSetBossWanderProps : BaseNode
+    {
+        public override void Init(RectTransform parentTf)
+        {
+            _nodeType = NodeType.BossSetWanderProps;
+            base.Init(parentTf);
+            _functionImg.sprite = ResourceManager.GetInstance().GetSprite("NodeIcon", "setwanderprops");
+        }
+
+        public override void CreateDefaultAttrs()
+        {
+            BaseNodeAttr nodeAttr;
+            // Boss
+            nodeAttr = NodeManager.CreateNodeAttr(NodeAttrType.Any);
+            nodeAttr.Init(this, "Unit", null);
+            attrs.Add(nodeAttr);
+            // X Range
+            nodeAttr = NodeManager.CreateNodeAttr(NodeAttrType.Any);
+            nodeAttr.Init(this, "X Range", null);
+            attrs.Add(nodeAttr);
+            // Y Range
+            nodeAttr = NodeManager.CreateNodeAttr(NodeAttrType.Any);
+            nodeAttr.Init(this, "Y Range", null);
+            attrs.Add(nodeAttr);
+            // X Amplitude
+            nodeAttr = NodeManager.CreateNodeAttr(NodeAttrType.Any);
+            nodeAttr.Init(this, "X Amplitude", null);
+            attrs.Add(nodeAttr);
+            // Y Amplitude
+            nodeAttr = NodeManager.CreateNodeAttr(NodeAttrType.Any);
+            nodeAttr.Init(this, "Y Amplitude", null);
+            attrs.Add(nodeAttr);
+            // Movement mode
+            nodeAttr = NodeManager.CreateNodeAttr(NodeAttrType.MoveMode);
+            nodeAttr.Init(this, "Movement Mode", null);
+            attrs.Add(nodeAttr);
+            // Direction Mode
+            nodeAttr = NodeManager.CreateNodeAttr(NodeAttrType.DirectionMode);
+            nodeAttr.Init(this, "Direction Mode", null);
+            attrs.Add(nodeAttr);
+        }
+
+        public override string GetNodeName()
+        {
+            return "set boss wander properties";
+        }
+
+        public override string ToDesc()
+        {
+            return string.Format("{0} wander range of({1}),({2}),X Amplitude of {3},Y Amplitude of {4},{5},{6}", 
+                GetAttrByIndex(0).GetValueString(), 
+                GetAttrByIndex(1).GetValueString(), GetAttrByIndex(2).GetValueString(),
+                GetAttrByIndex(3).GetValueString(),
+                GetAttrByIndex(4).GetValueString(),
+                GetAttrByIndex(5).GetValueString(),
+                GetAttrByIndex(5).GetValueString());
+        }
+
+        public override string ToLuaHead()
+        {
+            return string.Format("{0}:SetWanderRange({1},{2})\n{0}:SetWanderAmplitude({3},{4})\n{0}:SetWanderMode({5},{6})\n",
+                GetAttrByIndex(0).GetValueString(),
+                GetAttrByIndex(1).GetValueString(), GetAttrByIndex(2).GetValueString(),
+                GetAttrByIndex(3).GetValueString(), GetAttrByIndex(4).GetValueString(),
+                GetAttrByIndex(5).GetValueString(), GetAttrByIndex(6).GetValueString());
+        }
+    }
+}
